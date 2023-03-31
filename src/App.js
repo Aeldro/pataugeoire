a83dfe4ce222180ad7d6984e7fd866e361612803
ANTOINE_CASSAGNE <cassagnea@gmail.com>
2023-03-31 11:08:51 +0200

    m app-container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
-import './App.css';
-import NavBar from './components/NavBar';
-import PokemonCard from './components/PokemonCard';
-import PreviousNext from './components/PreviousNext';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import PokemonCard from "./components/PokemonCard";
+import PreviousNext from "./components/PreviousNext";
+import { useState } from "react";
 
+
+
+
+PokemonCard.propTypes = {
+  pokemonList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+
+  const pokemonList = [
+    {
+      name: "bulbizarre",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+
+    {
+      name: "salamèche",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "carapuce",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+  ];
+
+};
 function App() {
-  return (
-    <div>
+  
+   const [pokemonIndex, setPokemonIndex] = useState(0)
+  const pokemon = pokemonList[pokemonIndex]
+  const pokemonListLength = pokemonList.length;
 
+
+  return (
+    <div className="AppContainer">
+      ;<div ClassName="navbar "></div>
+      <div className="content"></div>
+      <div className="card">
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      </div>
     </div>
   );
 }
