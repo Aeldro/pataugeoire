7b8c640b4cbdf8039b8a9740b82fa7c7093652f6
Nolan DE PUYDT <nodepuydt@gmail.com>
2023-03-31 10:56:03 +0200

    Added previous and next buttons

--- src/components/PreviousNext.jsx
+++ src/components/PreviousNext.jsx
@@ -1,3 +1,22 @@
-const PreviousNext = () => {};
+const PreviousNext = ({ pokemonList, pokemonIndex, setPokemonIndex }) => {
+  const handlePreviousClick = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
+  const handleNextClick = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  return (
+    <>
+      {pokemonList[pokemonIndex] > 0 && (
+        <input type="button" value="Previous" onClick={handlePreviousClick} />
+      )}
+      {pokemonList[pokemonIndex] < pokemonList.length - 1 && (
+        <input type="button" value="Next" onClick={handleNextClick} />
+      )}
+    </>
+  );
+};
 
 export default PreviousNext;
