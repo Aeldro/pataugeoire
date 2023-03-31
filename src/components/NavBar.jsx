9017a7502b1f25e6f9881951b4551975064a97c5
Nolan DE PUYDT <nodepuydt@gmail.com>
2023-03-31 12:20:12 +0200

    Fixed everything

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,14 @@
 const NavBar = ({ pokemonList, pokemonIndex, setPokemonIndex }) => {
-
-    const handleClick = (index) => {
-        setPokemonIndex(index)
-      }
-    return (
-        <nav>
-            {pokemonList.map((pokemon, index) => {
-                <button onClick={() => handleClick(index)}>{pokemon.name}</button>
-                })}
-        </nav>
-    )
-
+  const handleClick = (index) => {
+    setPokemonIndex(index);
+  };
+  return (
+    <nav>
+      {pokemonList.map((pokemon, index) => (
+        <button onClick={() => handleClick(index)}>{pokemon.name}</button>
+      ))}
+    </nav>
+  );
 };
 
 export default NavBar;
