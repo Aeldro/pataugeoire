0a286655b1d0bef0486ff66b4f4fb57bb50962a2
Alain B <almasyser@wanadoo.fr>
2023-03-31 11:10:01 +0200

    pokemoncard 1er commit

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,12 @@
-const PokemonCard = () => {};
-
+function PokemonCard(props){
+    // console.log(props)
+    const {imgScr, name} = props
+    return (
+        <figure className="card">
+            {imgSrc? <img src={imgSrc} alt="Image" className="card-img"/> : <p>????</p>}
+            <figcaption>{name}</figcaption>
+        </figure>
+    )
+    
+}
 export default PokemonCard;
